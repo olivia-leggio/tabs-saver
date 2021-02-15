dff911ab5aab9e6217b7ce4457dad8042d1ee5c1
Olivia Leggio <o.r.legg12@gmail.com>
2021-02-15 00:01:01 -0500

    logs load and save to console, detects incorrectly

--- tabs-saver.js
+++ tabs-saver.js
@@ -1,1 +1,37 @@
-document.body.style.border = "5px solid red";
+/*
+function saveTabs() {
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then(sendSave).catch(onError);
+}
+
+function loadTabs() {
+    window.alert("loading!");
+}
+
+
+
+function sendSave(tabs) {
+    for (let tab of tabs) {
+        browser.tabs.sendMessage(
+            tab.id,
+            {greeting: "saving!"}
+        ).catch(onError);
+    }
+}
+
+
+function onError(error) {
+    console.error(`Error: ${error}`);
+  }
+*/
+document.getElementById("save").addEventListener("click", saveTabs);
+document.getElementById("restore").addEventListener("click", loadTabs());
+
+function saveTabs(){
+    console.log("S A V I N G");
+}
+function loadTabs(){
+    console.log("L O A D I N G");
+}
